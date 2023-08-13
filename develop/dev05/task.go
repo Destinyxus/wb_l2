0fba89c52022b0dfc296a82183615f9fc53ae04e
Vladimir <vldmrbusiness@gmail.com>
2023-08-14 01:26:14 +0300

    fixes

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -13,6 +13,10 @@ var (
 	flagB int
 	flagF bool
 	flagC int
+	flagc bool
+	flagI bool
+	flagV bool
+	flagN bool
 )
 
 func main() {
@@ -21,6 +25,11 @@ func main() {
 	flag.IntVar(&flagB, "B", 1, "before")
 	flag.BoolVar(&flagF, "F", false, "fixed")
 	flag.IntVar(&flagC, "C", 1, "context")
+	flag.BoolVar(&flagc, "c", false, "count")
+	flag.BoolVar(&flagI, "i", false, "registerIgnore")
+	flag.BoolVar(&flagV, "v", false, "invert")
+	flag.BoolVar(&flagN, "n", false, "lineNumber")
+
 	flag.Parse()
 
 	args := flag.Args()
@@ -29,10 +38,21 @@ func main() {
 		fmt.Println("Enter the keyword with args")
 		return
 	}
-
 	keyword := args[len(args)-1]
 
 	lines := readLines()
+
+	if flagc {
+		fmt.Println(SearchAmount(lines, keyword))
+	}
+
+	if flagV {
+		fmt.Println(exclude(lines, keyword))
+	}
+
+	if flagN {
+		fmt.Println(lineNum(lines, keyword))
+	}
 
 	switch os.Args[len(os.Args)-3] {
 	case "-A":
@@ -48,12 +68,77 @@ func main() {
 
 }
 
+func lineNum(arr []string, keyword string) int {
+	var num int
+
+	var found bool
+	if flagI {
+		arr, keyword = isRegister(arr, keyword)
+	}
+
+	for _, v := range arr {
+		num++
+		if isFixed(v, keyword) {
+			found = true
+			break
+		}
+
+	}
+
+	if !found {
+		fmt.Println("Keyword not found in the input.")
+	}
+	return num
+}
+
+func exclude(arr []string, keyword string) []string {
+
+	newArr := make([]string, 0)
+
+	if flagI {
+		arr, keyword = isRegister(arr, keyword)
+	}
+
+	for _, v := range arr {
+		if !isFixed(v, keyword) {
+			newArr = append(newArr, v)
+		}
+
+	}
+	return newArr
+
+}
+
+func SearchAmount(arr []string, keyword string) int {
+
+	var counter int
+	var found bool
+
+	if flagI {
+		arr, keyword = isRegister(arr, keyword)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		if isFixed(arr[i], keyword) {
+			found = true
+			counter++
+		}
+	}
+
+	if !found {
+		fmt.Println("Keyword not found in the input.")
+	}
+	return counter
+}
+
 func SearchWithLinesBefore(arr []string, keyword string, n int) []string {
 	var foundLines []string
 	var linesBefore []string
 	var found bool
 	//var before bool
-
+	if flagI {
+		arr, keyword = isRegister(arr, keyword)
+	}
 	for i := 0; i < len(arr); i++ {
 		if isFixed(arr[i], keyword) {
 			found = true
@@ -80,7 +165,9 @@ func SearchWithLinesBefore(arr []string, keyword string, n int) []string {
 func SearchWithLinesAfter(arr []string, keyword string, n int) []string {
 	var foundLines []string
 	var found bool
-
+	if flagI {
+		arr, keyword = isRegister(arr, keyword)
+	}
 	for i := 0; i < len(arr); i++ {
 		if isFixed(arr[i], keyword) {
 			found = true
@@ -114,6 +201,15 @@ func isFixed(s, substr string) bool {
 		return s == substr
 	}
 	return strings.Contains(s, substr)
+}
+
+func isRegister(arr []string, key string) ([]string, string) {
+	for i := range arr {
+		arr[i] = strings.ToLower(arr[i])
+	}
+	newKey := strings.ToLower(key)
+
+	return arr, newKey
 }
 
 func readLines() []string {
---
+++ develop/dev06/task.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	flagF     string
+	flagD     string
+	flagS     bool
+	fieldNums []int
+)
+
+func main() {
+	flag.BoolVar(&flagS, "s", false, "only-delimited")
+	flag.StringVar(&flagD, "d", "\t", "delimiter")
+	flag.StringVar(&flagF, "f", "", "field")
+	flag.Parse()
+
+	lines := readLines()
+
+	parseFieldNumbers()
+
+	if len(flagF) > 0 && flagD != "\t" {
+		cutByDelimiter(lines, flagD)
+	} else {
+		fmt.Println(strings.Join(lines, "\n"))
+	}
+
+}
+
+func cutByDelimiter(arr []string, delimiter string) {
+	var fields []string
+
+	for _, line := range arr {
+		parts := strings.Split(line, delimiter)
+
+		if flagS && !containsDelimiter(parts) {
+			continue
+		}
+		var extractedFields []string
+
+		for _, fieldNum := range fieldNums {
+
+			if fieldNum > 0 && fieldNum <= len(parts) {
+				extractedFields = append(extractedFields, strings.TrimSpace(parts[fieldNum-1]))
+			}
+
+		}
+		if len(extractedFields) > 0 {
+			fields = append(fields, strings.Join(extractedFields, ", "))
+		}
+	}
+
+	if len(fields) > 0 {
+		fmt.Println(strings.Join(fields, "\n"))
+	}
+}
+
+func containsDelimiter(parts []string) bool {
+	for _, part := range parts {
+		if strings.TrimSpace(part) != "" {
+			return true
+		}
+	}
+	return false
+}
+
+func parseFieldNumbers() {
+	fields := strings.Split(flagF, ",")
+	for _, field := range fields {
+		num := strings.TrimSpace(field)
+		if num != "" {
+			n, err := strconv.Atoi(num)
+			if err != nil {
+				fmt.Println("Invalid field number:", err)
+				os.Exit(1)
+			}
+			fieldNums = append(fieldNums, n)
+		}
+	}
+}
+
+func readLines() []string {
+	var lines []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+
+	return lines
+}
